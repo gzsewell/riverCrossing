e72829c10b20a72f259c1e3c7d49049c97c4b30b
gzsewell <gzsewell@gmail.com>
2025-01-13 13:21:21 -0600

    Block 20 guided practice: Using methods to move a sheep from the startBank to the targetBank.

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
+const form = document.querySelector("form");
+const sheepInput = document.querySelector("#numSheep");
 // === State ===
 const state = {
   start: ["sheep"],
   target: [],
 };
 
+let numSheepToAdd = 0;
+
+sheepInput.addEventListener("change", function (e) {
+  numSheepToAdd = e.target.value;
+});
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  for (let i = 0; i < numSheepToAdd; i++) {
+    state.start.push("sheep");
+  }
+  render();
+  console.log("Num sheep to add ", numSheepToAdd);
+});
+
 /** Moves a sheep from start to target */
 function moveSheep() {
   // TODO
+  const sheepToMove = state.start.pop();
+  state.target.push(sheepToMove);
+  render();
 }
 
 // === Render ===
@@ -21,7 +40,7 @@ function renderStartSheep() {
     li.append(button);
 
     // TODO: Add event listener so the sheep moves when clicked
-
+    li.addEventListener("click", function () {});
     return li;
   });
 
@@ -31,6 +50,20 @@ function renderStartSheep() {
 
 /** Renders sheep on the target bank */
 function renderTargetSheep() {
+  const targetSheep = state.start.map((sheep) => {
+    const li = document.createElement("li");
+
+    const button = document.createElement("button");
+    button.textContent = "🐑";
+    li.append(button);
+
+    // TODO: Add event listener so the sheep moves when clicked
+    li.addEventListener("click", function () {});
+    return li;
+  });
+
+  const targetSheepBank = document.querySelector("#startingBank ul");
+  targetBank.replaceChildren(...targetSheep);
   // TODO
 }
 
